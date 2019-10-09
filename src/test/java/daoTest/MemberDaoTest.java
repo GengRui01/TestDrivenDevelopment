ca1fcff0766c1bfc0fadd9aed43e79ac2c603335
gengrui01 <1207198609@qq.com>
2019-10-09 18:02:02 +0800

    解决mapper注入失败问题

--- src/test/java/daoTest/MemberDaoTest.java
+++ src/test/java/daoTest/MemberDaoTest.java
@@ -1,9 +1,15 @@
 package daoTest;
 
+import TestDrivenDevelopment.Application;
 import dao.MemberDao;
 import domain.entity.Member;
 import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Description: Member持久层测试
@@ -12,18 +18,22 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Author: GengRui
  * @Date: 2019/10/8 15:23
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class MemberDaoTest extends TestCase {
     @Autowired
     private MemberDao memberDao;
 
+    @Test
+    @Transactional
     public void testInsertMember() throws Exception {
         Member member = new Member();
-        member.setUsername("Tony");
+        member.setUsername("Peter");
         member.setGender(Boolean.FALSE);
-        member.setBirthday("1996-04-02");
-        member.setMemberId("6");
+        member.setBirthday("2005-06-18");
+        member.setMemberId("9");
         memberDao.insertMember(member);
-        Member testMember = memberDao.selectMemberById("6");
+        Member testMember = memberDao.selectMemberById("9");
         assertEquals(member.getUsername(), testMember.getUsername());
         assertEquals(member.getGender(), testMember.getGender());
         assertEquals(member.getBirthday(), testMember.getBirthday());
